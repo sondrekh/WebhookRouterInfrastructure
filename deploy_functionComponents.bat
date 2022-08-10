117381475c1fbd00a1e14406bfca0ffe8966fa7d
Sondre <25792547+sondrekh@users.noreply.github.com>
2022-08-10 17:21:55 +0200

    delete junk

--- deploy_functionComponents.bat
+++
@@ -1,7 +0,0 @@
-set resourceGroup=rg-2022-08-09
-
-az deployment group create ^
-  --name functionDeploy ^
-  --resource-group %resourceGroup% ^
-  --template-file functionContent.bicep ^
-  --parameters storageAccountType=Standard_LRS
--- deploy_rg.bat
+++
@@ -1,7 +0,0 @@
-set resourceGroup=rg-2022-08-09
-
-az deployment sub create ^
-    --location northeurope^
-    --template-file resourceGroup.bicep ^
-    --parameters ^
-        name=%resourceGroup%
